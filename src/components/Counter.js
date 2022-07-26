e2ac9207b0a51e87b16a992ee1ac959d0163e050
imkarthi16 <karthiveera@gmail.com>
2022-07-26 17:31:06 +0530

    update react toolkit

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,34 +1,36 @@
-import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import classes from "./Counter.module.css";
+import { useSelector, useDispatch } from 'react-redux';
+
+import { counterActions } from '../store/counter';
+import classes from './Counter.module.css';
 
 const Counter = () => {
   const dispatch = useDispatch();
-  const counter = useSelector((state) => state.counter);
-  const show = useSelector((state) => state.showCounter);
+  const counter = useSelector((state) => state.counter.counter);
+  const show = useSelector((state) => state.counter.showCounter);
 
   const incrementHandler = () => {
-    dispatch({ type: "increment" });
+    dispatch(counterActions.increment());
   };
 
-  const increase = () => {
-    dispatch({ type: "increase", amount: 5 });
+  const increaseHandler = () => {
+    dispatch(counterActions.increase(10)); // { type: SOME_UNIQUE_IDENTIFIER, payload: 10 }
   };
 
   const decrementHandler = () => {
-    dispatch({ type: "decrement" });
+    dispatch(counterActions.decrement());
   };
 
   const toggleCounterHandler = () => {
-    dispatch({ type: "toggle" });
+    dispatch(counterActions.toggleCounter());
   };
+
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
-        <button onClick={increase}>Increase 5</button>
+        <button onClick={increaseHandler}>Increase by 10</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,12 +1,11 @@
-import React from 'react';
 import classes from './UserProfile.module.css';
 
 const UserProfile = () => {
   return (
     <main className={classes.profile}>
-        <h2>My user Profile</h2>
+      <h2>My User Profile</h2>
     </main>
-  )
-}
+  );
+};
 
-export default UserProfile
+export default UserProfile;
